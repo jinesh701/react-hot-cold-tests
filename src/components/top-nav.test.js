a6f9045b22908c19bb09de492c6082d07ab244e0
Jinesh Patel <jinesh701@gmail.com>
2018-01-30 21:11:59 -0500

    added TopNav tests

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -7,4 +7,15 @@ describe("<TopNav />", () => {
   it("Renders without crashing", () => {
     shallow(<TopNav />);
   });
+
+  it("Should call onRestartGame when new game is clicked", () => {
+    const callback = jest.fn();
+    const wrapper = shallow(<TopNav onRestartGame={callback} />);
+    const newGame = wrapper.find(".new");
+
+    newGame.simulate("click", {
+      preventDefault() {}
+    });
+    expect(callback).toHaveBeenCalled();
+  });
 });
