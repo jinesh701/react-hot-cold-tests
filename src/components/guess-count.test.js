56ff1814dd8b6265d674e9cca9306d1bc5ae832d
Jinesh Patel <jinesh701@gmail.com>
2018-01-30 20:21:43 -0500

    added GuessCount tests

--- src/components/guess-count.test.js
+++ src/components/guess-count.test.js
@@ -7,4 +7,12 @@ describe("<GuessCount />", () => {
   it("Renders without crashing", () => {
     shallow(<GuessCount />);
   });
+
+  it("Renders correct guess count", () => {
+    let count = 2;
+
+    let wrapper = shallow(<GuessCount guessCount={count} />);
+
+    expect(wrapper.text()).toEqual(`You've made ${count} guesses!`);
+  });
 });
