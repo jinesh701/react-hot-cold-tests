59acd78363b83138c8f2fba87fdc34b637b83c0e
Jinesh Patel <jinesh701@gmail.com>
2018-01-30 21:14:07 -0500

    added GuessList tests

--- src/components/guess-list.test.js
+++ src/components/guess-list.test.js
@@ -7,4 +7,12 @@ describe("<GuessList />", () => {
   it("Renders without crashing", () => {
     shallow(<GuessList guesses={[]} />);
   });
+
+  it("Generate a list of guesses", () => {
+    const values = [10, 14, 41];
+    const wrapper = shallow(<GuessList guesses={values} />);
+    const guessItem = wrapper.find("li");
+
+    expect(guessItem.length).toEqual(values.length);
+  });
 });
