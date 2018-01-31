17222e85a43f8de7af6f09b76a565e9ba1035270
Jinesh Patel <jinesh701@gmail.com>
2018-01-30 20:06:53 -0500

    updated AuralStatus tests

--- src/components/aural-status.test.js
+++ src/components/aural-status.test.js
@@ -7,4 +7,11 @@ describe("<AuralStatus />", () => {
   it("Renders without crashing", () => {
     shallow(<AuralStatus />);
   });
+
+  it("Renders an Aural Status", () => {
+    let test = "Test status";
+
+    let wrapper = shallow(<AuralStatus auralStatus={test} />);
+    expect(wrapper.contains(test)).toEqual(true);
+  });
 });
